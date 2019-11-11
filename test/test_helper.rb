a61bdba9d9a0af1aaa28ffc45d5b17a096614973
dathao <dathao262@gmail.com>
2019-11-11 21:27:02 +0700

    Make a basic User model (including secure passwords)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,15 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-<<<<<<< HEAD
-
-=======
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-end
->>>>>>> master
-
 class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
